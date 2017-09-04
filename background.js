8e6d253033eabcd1a1065a5312b973d619c79ea9
AthesDrake <athesdrake@gmail.com>
2017-09-04 15:40:37 +0200

    Fixed match

--- background.js
+++ background.js
@@ -1,15 +1,18 @@
-var xhttp   = new XMLHttpRequest();
+var xhttp = new XMLHttpRequest();
 var actived = true;
 
 xhttp.onreadystatechange = function() {
 	if (this.readyState==4 && this.status==200) {
-		var xhtml = this.responseText;
-		let match = /<a href="\/conversations">\d+ <img src="\/img\/icones\/16\/enveloppe.png" class="espace-2-2 img16"; title=".+"><\/a>/.exec(xhtml);
+		console.log(1)
+		var xhtml = this.responseText
+		let match = /<a href="\/conversations">\d+ <img src="\/img\/icones\/16\/enveloppe\.png" class="espace-2-2 img16" title="(\d+) nouveau\(x\) message\(s\)"><\/a>/.exec(xhtml)
+		console.log(match)
 		if (match!==null) {
+			console.log(2)
 			var notif = new Notification("Notifier801", {
-				body: "Vous avez "+/\d+/.exec(/>\d+/.exec(match))+" nouveau(x) message(s) !",
+				body: "Vous avez "+match[1]+" nouveau(x) message(s) !",
 				icon: "http://societe.atelier801.com/images/logo.png",
-			});
+			})
 			notif.onclick = function(event) {
 				event.preventDefault();
 				let fen = window.open('http://atelier801.com/conversations');
