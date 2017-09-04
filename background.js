aba5bf7b82a5fcfcaed2ce20e3b3fca5fc1b2406
AthesDrake <athesdrake@gmail.com>
2017-09-04 15:04:48 +0200

    Fixed window focus

--- background.js
+++ background.js
@@ -1,20 +1,20 @@
-var xhttp   = new XMLHttpRequest()
-var actived = true
+var xhttp   = new XMLHttpRequest();
+var actived = true;
 
 xhttp.onreadystatechange = function() {
 	if (this.readyState==4 && this.status==200) {
 		var xhtml = this.responseText;
-		let match = /<a href="\/conversations">\d+ <img src="\/img\/icones\/16\/enveloppe.png" class="espace-2-2 img16" title=".+"><\/a>/.exec(xhtml);
+		let match = /<a href="\/conversations">\d+ <img src="\/img\/icones\/16\/enveloppe.png" class="espace-2-2 img16"; title=".+"><\/a>/.exec(xhtml);
 		if (match!==null) {
 			var notif = new Notification("Notifier801", {
 				body: "Vous avez "+/\d+/.exec(/>\d+/.exec(match))+" nouveau(x) message(s) !",
 				icon: "http://societe.atelier801.com/images/logo.png",
-			})
+			});
 			notif.onclick = function(event) {
-				window.focus();
 				event.preventDefault();
-				window.open('http://atelier801.com/conversations')
-				event.currentTarget.close()
+				let fen = window.open('http://atelier801.com/conversations');
+				fen.focus();
+				event.currentTarget.close();
 			}
 		}
 	}
